14296923fbb325cc1c7e268a569810125e72a67d
Jaroslav Radimský <pokejara@seznam.cz>
2024-04-05 15:36:54 +0200

    msw prace

--- MSW/zápočtové úlohy/uloha_4.py
+++ MSW/zápočtové úlohy/uloha_4.py
@@ -6,11 +6,11 @@ from scipy import interpolate
 
 
 # sine function
-x = np.linspace(0, 2 * np.pi, 50)
+x = np.linspace(0, 2 * np.pi, 100)
 y = np.sin(x)
 
 
-x_interpol = np.linspace(0, 2 * np.pi, 10)
+x_interpol = np.linspace(0, 2 * np.pi, 6)
 y_interpol = np.sin(x_interpol)
 y_noised = y_interpol.copy()
 
@@ -30,22 +30,46 @@ y_lagrange = interpolate.lagrange(x_interpol, y_interpol)
 xphi = np.linspace(0, 2 * np.pi, 100)
 phi = y_lagrange(xphi)
 
+# BSpline interpolation
+tck = interpolate.splrep(x_interpol, y_interpol)
+x_bspline = np.linspace(0, 2 * np.pi, 100)
+y_bspline = interpolate.splev(x_bspline, tck)
 
-plt.plot(xphi, phi, "m-")
+
 plt.plot(xnew, ynew, "r-")
+plt.plot(xphi, phi, "m-")
+plt.plot(x_bspline, y_bspline, "y-")
 plt.plot(x_interpol, y_interpol, "b.")
 # plt.plot(x, y, "g-")
+plt.legend(["1-D interpolation", "Lagrange interpolation", "BSpline interpolation" , "Data"])
 plt.title("Sine")
 plt.show()
+
+# porovnání přesnosti interpolace pomocí součtu čtverců (rozptyylů)
+ctverce_1D = 0
+for i in range(len(y)):
+    ctverce_1D += (y[i] - y_1d(x)[i])**2
+
+ctverce_lagrange = 0
+for i in range(len(y)):
+    ctverce_lagrange += (y[i] - y_lagrange(x)[i])**2
+
+ctverce_bspline = 0
+for i in range(len(y)):
+    ctverce_bspline += (y[i] - y_bspline[i])**2
+
+print("1-D interpolation: ", ctverce_1D)
+print("Lagrange interpolation: ", ctverce_lagrange)
+print("BSpline interpolation: ", ctverce_bspline)
 
 
 
 
 # logarithm function
-x = np.linspace(1, 2 * np.pi, 50)
+x = np.linspace(1, 2 * np.pi, 100)
 y = np.log2(x)
 
-x_interpol = np.linspace(1, 2 * np.pi, 10)
+x_interpol = np.linspace(1, 2 * np.pi, 5)
 y_interpol = np.log2(x_interpol)
 y_noised = y_interpol.copy()
 
@@ -56,36 +80,6 @@ for i in range(len(y_noised)):
 
 # 1-D interpolation
 y_1d = interpolate.interp1d(x_interpol, y_interpol)
-xnew = np.linspace(1, 2 * np.pi, 10)
-ynew = y_1d(xnew)
-
-# Lagrange interpolation
-y_lagrange = interpolate.lagrange(x_interpol, y_interpol)
-xphi = np.linspace(1, 2 * np.pi, 100)
-phi = y_lagrange(xphi)
-
-
-plt.plot(xphi, phi, "m-")
-plt.plot(xnew, ynew, "r-")
-plt.plot(x_interpol, y_interpol, "b.")
-plt.plot(x, y, "g-")
-plt.title("Logarithm")
-plt.show()
-
-# square root function
-x = np.linspace(1, 2 * np.pi, 50)
-y = np.sqrt(x)
-
-x_interpol = np.linspace(1, 2 * np.pi, 10)
-y_interpol = np.sqrt(x_interpol)
-y_noised = y_interpol.copy()
-
-# add noise
-for i in range(len(y_noised)):
-    y_noised[i] += random.uniform(-0.1, 0.1)
-
-# 1-D interpolation
-y_1d = interpolate.interp1d(x, y_noised)
 xnew = np.linspace(1, 2 * np.pi, 100)
 ynew = y_1d(xnew)
 
@@ -94,12 +88,87 @@ y_lagrange = interpolate.lagrange(x_interpol, y_interpol)
 xphi = np.linspace(1, 2 * np.pi, 100)
 phi = y_lagrange(xphi)
 
+# BSpline interpolation
+tck = interpolate.splrep(x_interpol, y_interpol)
+x_bspline = np.linspace(1, 2 * np.pi, 100)
+y_bspline = interpolate.splev(x_bspline, tck)
 
-plt.plot(xphi, phi, "m-")
 plt.plot(xnew, ynew, "r-")
+plt.plot(xphi, phi, "m-")
+plt.plot(x_bspline, y_bspline, "y-")
 plt.plot(x_interpol, y_interpol, "b.")
-plt.plot(x, y, "g-")
-plt.title("Exponential")
+# plt.plot(x, y, "g-")
+plt.legend(["1-D interpolation", "Lagrange interpolation", "BSpline interpolation" , "Data"])
+plt.title("Logarithm")
 plt.show()
 
+
+ctverce_1D = 0
+for i in range(len(y)):
+    ctverce_1D += (y[i] - y_1d(x)[i])**2
+
+ctverce_lagrange = 0
+for i in range(len(y)):
+    ctverce_lagrange += (y[i] - y_lagrange(x)[i])**2
+
+ctverce_bspline = 0
+for i in range(len(y)):
+    ctverce_bspline += (y[i] - y_bspline[i])**2
+
+print("1-D interpolation: ", ctverce_1D)
+print("Lagrange interpolation: ", ctverce_lagrange)
+print("BSpline interpolation: ", ctverce_bspline)
+
+
+# square root function
+x = np.linspace(1, 2 * np.pi, 100)
+y = np.sqrt(x)
+
+x_interpol = np.linspace(1, 2 * np.pi, 5)
+y_interpol = np.sqrt(x_interpol)
+y_noised = y_interpol.copy()
+
+# add noise
+for i in range(len(y_noised)):
+    y_noised[i] += random.uniform(-0.1, 0.1)
+
+# 1-D interpolation
+y_1d = interpolate.interp1d(x_interpol, y_interpol)
+xnew = np.linspace(1, 2 * np.pi, 100)
+ynew = y_1d(xnew)
+
+# Lagrange interpolation
+y_lagrange = interpolate.lagrange(x_interpol, y_interpol)
+xphi = np.linspace(1, 2 * np.pi, 100)
+phi = y_lagrange(xphi)
+
+# BSpline interpolation
+tck = interpolate.splrep(x_interpol, y_interpol)
+x_bspline = np.linspace(1, 2 * np.pi, 100)
+y_bspline = interpolate.splev(x_bspline, tck)
+
+plt.plot(xnew, ynew, "r-")
+plt.plot(xphi, phi, "m-")
+plt.plot(x_bspline, y_bspline, "y-")
+plt.plot(x_interpol, y_interpol, "b.")
+# plt.plot(x, y, "g-")
+plt.legend(["1-D interpolation", "Lagrange interpolation", "BSpline interpolation" , "Data"])
+plt.title("Square root")
+plt.show()
+
+ctverce_1D = 0
+for i in range(len(y)):
+    ctverce_1D += (y[i] - y_1d(x)[i])**2
+
+ctverce_lagrange = 0
+for i in range(len(y)):
+    ctverce_lagrange += (y[i] - y_lagrange(x)[i])**2
+
+ctverce_bspline = 0
+for i in range(len(y)):
+    ctverce_bspline += (y[i] - y_bspline[i])**2
+
+print("1-D interpolation: ", ctverce_1D)
+print("Lagrange interpolation: ", ctverce_lagrange)
+print("BSpline interpolation: ", ctverce_bspline)
 
---
+++ MSW/zápočtové úlohy/uloha_5.py
@@ -0,0 +1,84 @@
+import numpy as np
+from scipy import optimize
+import matplotlib.pyplot as plt
+from functools import wraps
+import time
+
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        return (end - start) * 1000, result
+    return wrapper
+
+@timer
+def bisect(f, a, b):
+    return optimize.bisect(f, a, b)
+
+@timer
+def newton(f, x0):
+    return optimize.newton(f, x0)
+
+
+# funkce 1
+print("Funkce 1: x^2 - 4")
+def f1(x):
+    return x**2 - 4
+
+a0 = 0
+b0 = 4
+
+res = 4**(1/2)
+
+# bisekce
+cas, x = bisect(f1, a0, b0)
+print(f"Bisekce: {x}, čas: {cas} ms, chyba: {abs(res - x)}")
+
+
+# newton
+cas, x = newton(f1, x0=(a0+b0)/2)
+print(f"Newton: {x}, čas: {cas} ms, chyba: {abs(res - x)}")
+
+
+# funkce 2
+print("\nFunkce 2: x^3 - 4")
+def f2(x):
+    return np.exp(x) - 4
+
+a0 = 0
+b0 = 4
+
+
+res = np.log(4)
+
+# bisekce
+cas, x = bisect(f2, a0, b0)
+print(f"Bisekce: {x}, čas: {cas} ms, chyba: {abs(res - x)}")
+
+
+# newton
+cas, x = newton(f2, x0=(a0+b0)/2)
+print(f"Newton: {x}, čas: {cas} ms, chyba: {abs(res - x)}")
+
+
+# funkce 3
+print("\nFunkce 3: 2 * sin(x) - 0.5")
+def f3(x):
+    return 2 * np.sin(x) - 0.5
+
+a0 = -2
+b0 = 2
+
+res = np.arcsin(0.25)
+
+# bisekce
+cas, x = bisect(f3, a0, b0)
+print(f"Bisekce: {x}, čas: {cas} ms, chyba: {abs(res - x)}")
+
+
+# newton
+cas, x = newton(f3, x0=(a0+b0)/2)
+print(f"Newton: {x}, čas: {cas} ms, chyba: {abs(res - x)}")
